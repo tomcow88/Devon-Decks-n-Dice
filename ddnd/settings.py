491a62d258fe237accb24ea1504dc702ccfc6158
Tom Cowen <tomreescowen@gmail.com>
2024-03-14 01:21:03 +0000

    Add Event model to events/models.py

--- ddnd/settings.py
+++ ddnd/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'events',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -91,6 +93,11 @@ WSGI_APPLICATION = 'ddnd.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- events/models.py
+++ events/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Event(models.Model):
+    """
+    Stores a single event entry related to :model:`auth.User`
+    """
+    name = models.CharField(max_length=200, unique=True)
+    excerpt = models.TextField(blank=True)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    max_attendees = models.IntegerField()
+    attendees = models.ManyToManyField(User)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-event_date"]
+
+    def __str__(self):
+        return f"{self.name} on {self.event_date}"
