5d6140b7f5b63d2f941e16c2788d40cb8f529a0b
Tom Cowen <tomreescowen@gmail.com>
2024-03-15 23:40:54 +0000

    Fix double booking issue

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import redirect, render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 from .models import Table, Booking
 from .forms import BookingForm
@@ -13,13 +14,17 @@ def create_booking(request):
     if request.method == 'POST':
         booking_form = BookingForm(data=request.POST)
         if booking_form.is_valid():
-            booking = booking_form.save(commit=False)
-            booking.name = request.user
-            booking.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Your booking was created successfully!'
-            )
+            try:
+                booking = booking_form.save(commit=False)
+                booking.name = request.user
+                booking.save()
+                messages.add_message(
+                    request, messages.SUCCESS,
+                    'Your booking was created successfully!'
+                )
+                return redirect('bookings')
+            except ValidationError as e:
+                messages.error(request, e.message)
         else:
             messages.add_message(request, messages.ERROR,
                                  'Please try again.')
