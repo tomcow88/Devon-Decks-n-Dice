27b1b35c6f4bcbb37d0629324f972d31dbecc550
Tom Cowen <tomreescowen@gmail.com>
2024-03-15 19:05:32 +0000

    Add styling to account pages

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.create_booking, name='create_booking'),
+    path('', views.create_booking, name='bookings'),
 ]
