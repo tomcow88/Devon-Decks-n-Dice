7e51c8829444aefa7ed2c03c907d488415a38e2e
Tom Cowen <tomreescowen@gmail.com>
2024-03-16 14:30:29 +0000

    Add cafe-interior jpeg to static folder

--- ddnd/urls.py
+++ ddnd/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("bookings/", include("bookings.urls"), name="bookings-urls"),
+    path("library/", include("library.urls"), name="library-urls"),
     path("", include("events.urls"), name="events-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def library(request):
+    response=requests.get('https://api.geekdo.com/xmlapi/search').json()
+    return render(request, 'game_library.html', {'response':response})
