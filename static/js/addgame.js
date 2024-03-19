db5cfb7720c5ee6e41511a838f6f31cba49eed8f
Tom Cowen <tomreescowen@gmail.com>
2024-03-19 14:06:10 +0000

    add onsubmit to game form on search results and remove event listener from addgame.js, collectstatic

--- static/js/addgame.js
+++ static/js/addgame.js
@@ -1,17 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Attach an event listener to each form with IDs starting with "game-form-"
-    document.querySelectorAll('form[id^="game-form-"]').forEach(form => {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault(); // Prevent the form from submitting the traditional way
-
-            const bgg_id = this.id.replace('game-form-', ''); // Extract bgg_id from the form's ID
-
-            // Call the function to handle the AJAX submission
-            addGameToDatabase(bgg_id);
-        });
-    });
-});
-
 function addGameToDatabase(bgg_id) {
     const form = document.getElementById(`game-form-${bgg_id}`);
     const formData = new FormData(form);
