2525fb98f9c6954f77fce704841a4fecf5010c14
Tom Cowen <tomreescowen@gmail.com>
2024-03-19 13:34:55 +0000

    collect static

--- staticfiles/js/addgame.js
+++ staticfiles/js/addgame.js
@@ -1,25 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('form[id^="game-form-"]').forEach(form => {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault(); // Prevent the default form submission action
-
-            const bgg_id = this.id.replace('game-form-', ''); // Extract the bgg_id from the form's id
-
-            addGameToDatabase(bgg_id); // Call the function to handle the form submission
-        });
-    });
-});
-
 function addGameToDatabase(bgg_id) {
     const form = document.getElementById(`game-form-${bgg_id}`);
     const formData = new FormData(form);
 
-    // Log each item in the FormData object
-    for (let [key, value] of formData.entries()) {
-        console.log(`${key}: ${value}`);
-    }
-
-    // Make the POST request to the server
     fetch('/library/add-game-to-database/', {
         method: 'POST',
         body: formData,
